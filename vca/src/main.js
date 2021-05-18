7f104415de73c787ca7dbd849ca9f072b422b70c
Nedim Begić <nedim@renesalis-packaging.com>
2021-05-18 13:37:24 +0200

    Modify main.js

--- vca/src/main.js
+++ vca/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueCompositionApi from '@vue/composition-api' // <-- Make the import
 
 Vue.config.productionTip = false
+
+Vue.use(VueCompositionApi) // <-- Tell Vue to use it
 
 new Vue({
   render: h => h(App)
